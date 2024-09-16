373cd86b4fd0cc47d9b69f7a91b912ae7b8e8b0c
Sonia <30045332@cc.peralta.edu>
2024-09-15 22:27:55 -0700

    Initial Commit

---
+++ class_activity6/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  class_activity6
+//
+//  Created by Sonia Lopchan on 9/15/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
