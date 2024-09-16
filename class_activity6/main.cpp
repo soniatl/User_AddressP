61dc174076f51c0053ab1750937b19ccd32a4141
soniatl <30045332@cc.peralta.edu>
2024-09-15 23:01:04 -0700

    Update main.cpp

--- class_activity6/main.cpp
+++ class_activity6/main.cpp
@@ -4,11 +4,30 @@
 //
 //  Created by Sonia Lopchan on 9/15/24.
 //
-
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    string street, city, state, zipcode;
+    
+    cout << "Enter your street name: " << endl;
+    getline(cin, street);
+    
+    cout << "Enter your city: " << endl;
+    getline(cin, city);
+    
+    cout << "Enter your state: " << endl;
+    getline(cin, state);
+    
+    cout << "Enter your zipcode: " << endl;
+    getline(cin, zipcode);
+    
+    //Display Address
+    cout << "Your address is: "<<street<<endl;
+    cout << city<<" "<<state<<" "<<zipcode<<" "<<endl;
+    
     return 0;
 }
